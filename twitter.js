5fba48331cfea82c620d4184c310a3b8b9ee40d4
kangsterizer <dump+github@tzib.net>
2020-05-11 23:29:47 -0700

    Add files via upload

--- twitter.js
+++ twitter.js
@@ -1,9 +1,7 @@
 var spamobserver = new MutationObserver(function (mutations, me) {
     var all = document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-aqfbo4.r-zso239.r-1hycxz > div");
     if (all) {
-        all.remove()
-        me.disconnect();
-        return;
+        all.remove();
     }
 });
 
